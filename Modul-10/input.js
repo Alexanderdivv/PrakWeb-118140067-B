8df4b239d6b2cb74e9b485e789204928db3c9c54
Alexanderdivv <alexanderdivagraelbangun@gmail.com>
2020-12-07 04:49:11 +0700

    perubahan input

--- Modul-10/input.js
+++ Modul-10/input.js
@@ -1,47 +1,45 @@
-// app = act
-// el = tampung
-
-
 let act = new function(){
 	this.tampung = document.getElementById('daftarBarang');
-	this.barang = ['Buku tulis', 'Pensil', 'Spidol'];
-	let pjg=this.barang.length;
+	this.daftarBarang = ['Buku tulis', 'Pensil', 'Spidol'];
 	this.FetchAll=function(){
 		var data = '';
-		if(pjg>0){
-			for (var i = 0;i < this.pjg; i++) {
+		if(this.daftarBarang.length>0){
+			for (i = 0;i < this.daftarBarang.length; i++) {
 				data +='<ul>';
-				data +='<li>' + this.barang[i]+'';
+				data +='<li>' + this.daftarBarang[i]+' ';
 				data +='<a href="#" onclick="act.editF('+i+')">[Edit </a>]';
-				data +='<a href="#" onclick="app.deleteF('+i+')"> | Delete]</a></li>';
+				data +='<a href="#" onclick="act.deleteF('+i+')"> | Delete]</a></li>';
 				data += '</ul>';
 			}
 		}
 		return this.tampung.innerHTML = data;
 	};
 	this.inputF = function () {
-	tampung = doocument.getElementByName('nama');
-	let barang = tampung.value;
+	tampung = document.getElementById('nama');
+	let daftarBarang = tampung.value;
 
-	if (barang) {
-		this.barang.push(barang.trim());
+	if (daftarBarang) {
+		this.daftarBarang.push(daftarBarang.trim());
 		tampung.value = '';
 		this.FetchAll();
 	}
 };
 	this.editF = function (item){
-		tampung = prompt('Ubah barang','');
-		this.barang[item]= tampung;
+		tampung = prompt('Ubah Barang','');
+		this.daftarBarang[item]= tampung;
 		this.FetchAll();
 	};
 
 	this.deleteF = function(item){
 		let notf = confirm('Yakin ?');
 		if (notf) {
-			this.barang.splice(item, 1);
+			this.daftarBarang.splice(item, 1);
 			this.FetchAll();
 		}
 	};
 }
 
 act.FetchAll();
+ function CloseInput() {
+    document.getElementById('spoiler').style.display = 'none';
+  }
